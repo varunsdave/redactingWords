c79eef7c2cafbbe9656390f6bd7d182bd016b505
varunsdave <varun.s.dave.public@gmail.com>
2019-02-10 10:41:29 -0500

    Provide exception

--- src/com/varunsdave/Main.java
+++ src/com/varunsdave/Main.java
@@ -17,18 +17,16 @@ public class Main {
         }
     }
 
-    static boolean isValidInputs(String[] args) throws IllegalArgumentException {
+    private static boolean isValidInputs(String[] args) throws IllegalArgumentException{
         if (args.length < 3) {
-            System.out.println("You must enter 3 arguments. \n" +
+            throw new IllegalArgumentException("You must enter 3 arguments. \n" +
                     "Argument 1: Path to a file containing readacting terms.\n" +
                     "Argument 2: Directory containing all the files to redact words from.\n" +
                     "Argument 3: Output file");
-            return false;
         }
         for (int i = 0; i < args.length; i++) {
             if (Files.notExists(Paths.get(args[i]))) {
-                System.out.println("Invalid argument: '" + args[i] + "' does not exist. System will exit.");
-                return false;
+               throw new IllegalArgumentException("Invalid argument: '" + args[i] + "' does not exist. System will exit.");
             }
         }
         return true;
